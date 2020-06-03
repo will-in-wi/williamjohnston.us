e9c1c7121eeff6da8969ba3d592e76a16e39601b
William Johnston <william@johnstonhaus.us>
2020-06-02 21:05:16 -0500

    Initial site

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+// const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+// const sourcemaps = require('gulp-sourcemaps');
+
+// Use dart-sass for modules.
+sass.compiler = require('sass');
+
+gulp.task('html', function () {
+  return gulp.src('content/**/*.+(html|nj)')
+    .pipe(nunjucksRender({
+      path: ['templates']
+    }))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('css', function () {
+  return gulp.src(['src/styles.scss'])
+    // .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    // .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/assets'))
+});
+
+gulp.task('files', function () {
+  return gulp.src('assets/**/*')
+    .pipe(gulp.dest('build/assets'))
+});
+
+// gulp.task('js', gulp.series(function () {
+//   return gulp.src(['node_modules/tiny-slider/dist/tiny-slider.js', 'src/*.js'])
+//     // .pipe(sourcemaps.init())
+//     .pipe(concat('app.min.js'))
+//     // .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('build/assets'))
+// }));
+
+gulp.task('htaccess', function () {
+  return gulp.src('htaccess')
+    .pipe(rename('.htaccess'))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('default', gulp.parallel([
+  'html',
+  'css',
+  'files',
+  // 'js',
+  'htaccess'
+]));
+
+gulp.task('watch', gulp.series('default', function () {
+  gulp.watch('content/**/*', gulp.series('html'));
+  gulp.watch('templates/**/*', gulp.series('html'));
+  gulp.watch('src/**/*', gulp.series(
+    'css' //,
+    // 'js'
+  ));
+  gulp.watch('assets/**/*', gulp.series('files'));
+}));
+
+gulp.task('serve', gulp.parallel('watch', function () {
+  const connect = require('connect'),
+    serveStatic = require('serve-static'),
+    serveIndex = require('serve-index');
+
+  const app = connect()
+    .use(serveStatic('build'))
+    .use(serveIndex('build'));
+
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
+}));
---
+++ readme.md
@@ -0,0 +1,19 @@
+# williamjohnston.us website
+
+## Quickstart
+
+To see the website, run `yarn serve` and visit http://localhost:9000
+
+To build the website, run `yarn build`. It'll be in the build folder.
+
+## Deployment
+
+TODO
+
+## Structure
+
+Built using Nunjucks templating for its Gulp support and Django-style inheritance.
+
+Using a rough MCSS structure for CSS.
+
+General principle of not importing libraries unless we can't help it. Sites like this should download it almost no time flat and be tiny. Let's deliver on that.
